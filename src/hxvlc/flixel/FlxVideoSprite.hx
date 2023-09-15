18998ae55422d7f914f5bc9419ae6fe016232413
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-16 02:05:02 +0300

    Yea

--- src/hxvlc/flixel/FlxVideoSprite.hx
+++ src/hxvlc/flixel/FlxVideoSprite.hx
@@ -19,12 +19,13 @@ class FlxVideoSprite extends FlxSprite
 
 		bitmap = new Video();
 		bitmap.alpha = 0;
+		#if FLX_SOUND_SYSTEM
 		bitmap.onOpening.add(function()
 		{
-			#if FLX_SOUND_SYSTEM
-			bitmap.volume = Math.floor((FlxG.sound.muted ? 0 : 1) * (FlxG.sound.volume * 100));
-			#end
+			bitmap.mute = FlxG.sound.muted;
+			bitmap.volume = Math.floor(FlxG.sound.volume * 100);
 		});
+		#end
 		bitmap.onFormatSetup.add(() -> loadGraphic(bitmap.bitmapData));
 
 		FlxG.game.addChild(bitmap);
@@ -78,7 +79,8 @@ class FlxVideoSprite extends FlxSprite
 	public override function update(elapsed:Float):Void
 	{
 		#if FLX_SOUND_SYSTEM
-		bitmap.volume = Math.floor((FlxG.sound.muted ? 0 : 1) * (FlxG.sound.volume * 100));
+		bitmap.mute = FlxG.sound.muted;
+		bitmap.volume = Math.floor(FlxG.sound.volume * 100);
 		#end
 
 		super.update(elapsed);
