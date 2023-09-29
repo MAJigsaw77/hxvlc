2487cecb84b30536faae988a3e21181cb0f500c5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-29 23:58:34 +0300

    Cool

--- samples/flixel/source/Main.hx
+++ samples/flixel/source/Main.hx
@@ -1,6 +1,11 @@
 package;
 
+#if android
+import android.content.Context;
+#end
 import flixel.FlxGame;
+import haxe.io.Path;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -9,6 +14,14 @@ class Main extends Sprite
 	{
 		super();
 
+		#if android
+		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
+		#end
+
 		addChild(new FlxGame(1280, 720, PlayState));
+
+		var overlay:FPS = new FPS(10, 10, 0xFFFFFFFF);
+		overlay.scaleX = overlay.scaleY = stage.window.scale;
+		addChild(overlay);
 	}
 }
