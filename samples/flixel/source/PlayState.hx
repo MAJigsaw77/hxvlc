05fe10a4c3ce961483be29e407d2731c7572616b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-30 00:03:31 +0300

    Updated the flixel sample

--- samples/flixel/source/PlayState.hx
+++ samples/flixel/source/PlayState.hx
@@ -1,20 +1,43 @@
 package;
 
+#if android
+import android.widget.Toast;
+#end
 import flixel.FlxState;
+import haxe.Exception;
 import hxvlc.flixel.FlxVideo;
+import openfl.utils.Assets;
+import sys.io.File;
+import sys.FileSystem;
+
+using haxe.io.Path;
 
 class PlayState extends FlxState
 {
 	override function create():Void
 	{
-		var video:FlxVideo = new FlxVideo();
-		video.onEndReached.add(video.dispose);
+		final path:String = 'assets/video.mp4';
 
 		#if android
-		video.play('https://github.com/MAJigsaw77/hxvlc/raw/main/samples/flixel/assets/video.mp4');
-		#else
-		video.play('assets/video.mp4');
+		try
+		{
+			if (!FileSystem.exists(path.directory()))
+			{
+				FileSystem.createDirectory(path.directory());
+
+				File.saveBytes(path, Assets.getBytes(path));
+			}
+			else if (!FileSystem.exists(path))
+				File.saveBytes(path, Assets.getBytes(path));
+				
+		}
+		catch (e:Exception)
+			Toast.makeText(e.message, Toast.LENGTH_LONG);
 		#end
+
+		var video:FlxVideo = new FlxVideo();
+		video.onEndReached.add(video.dispose);
+		video.play('assets/video.mp4');
 
 		super.create();
 	}
