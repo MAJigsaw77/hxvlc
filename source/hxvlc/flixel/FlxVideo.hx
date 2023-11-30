83a96893baed86671366166b094218f3170cdcd7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-11-30 14:32:23 +0200

    Update FlxVideo.hx

--- source/hxvlc/flixel/FlxVideo.hx
+++ source/hxvlc/flixel/FlxVideo.hx
@@ -34,7 +34,7 @@ class FlxVideo extends Video
 		FlxG.addChildBelowMouse(this);
 	}
 
-	public override function play(location:String, repeat:Int = 0):Bool
+	public override function load(location:String, repeat:Int = 0):Bool
 	{
 		if (FlxG.autoPause)
 		{
@@ -48,9 +48,9 @@ class FlxVideo extends Video
 		FlxG.stage.addEventListener(Event.ENTER_FRAME, onEnterFrame);
 
 		if (FileSystem.exists(Sys.getCwd() + location))
-			return super.play(Sys.getCwd() + location, repeat);
+			return super.load(Sys.getCwd() + location, repeat);
 
-		return super.play(location, repeat);
+		return super.load(location, repeat);
 	}
 
 	public override function dispose():Void
