3ba1e7553224a3b389f3898a0e4ed84c385f553d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-11-30 21:56:40 +0200

    Update FlxVideoSprite.hx

--- source/hxvlc/flixel/FlxVideoSprite.hx
+++ source/hxvlc/flixel/FlxVideoSprite.hx
@@ -44,14 +44,14 @@ class FlxVideoSprite extends FlxSprite
 	}
 
 	/**
-	 * Call this function to play a video.
+	 * Call this function to load a video.
 	 *
 	 * @param location The local filesystem path or the media location url.
 	 * @param repeat The number of times the video should repeat itself.
 	 *
-	 * @return `true` if the video started playing or `false` if there's an error.
+	 * @return `true` if the video loaded successfully or `false` if there's an error.
 	 */
-	public function play(location:String, repeat:Int = 0):Bool
+	public function load(location:String, repeat:Int = 0):Bool
 	{
 		if (bitmap == null)
 			return false;
@@ -66,9 +66,22 @@ class FlxVideoSprite extends FlxSprite
 		}
 
 		if (FileSystem.exists(Sys.getCwd() + location))
-			return bitmap.play(Sys.getCwd() + location, repeat);
+			return bitmap.load(Sys.getCwd() + location, repeat);
 
-		return bitmap.play(location, repeat);
+		return bitmap.load(location, repeat);
+	}
+
+	/**
+	 * Call this function to play a video.
+	 *
+	 * @return `true` if the video started playing or `false` if there's an error.
+	 */
+	public function play():Bool
+	{
+		if (bitmap == null)
+			return false;
+
+		return bitmap.play();
 	}
 
 	/**
