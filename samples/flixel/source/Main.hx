8803f37a759bd3fd3d9f15ce3718797fbd0b7cd0
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-12-27 08:06:03 +0200

    Update Main.hx

--- samples/flixel/source/Main.hx
+++ samples/flixel/source/Main.hx
@@ -8,8 +8,12 @@ import haxe.io.Path;
 import haxe.CallStack;
 import haxe.Exception;
 import haxe.Log;
-import lime.system.System;
+import lime.system.System as LimeSystem;
 import openfl.display.Sprite;
+import openfl.errors.Error;
+import openfl.events.ErrorEvent;
+import openfl.events.UncaughtErrorEvent;
+import openfl.system.System as OpenFLSystem;
 import openfl.Lib;
 import sys.io.File;
 import sys.FileSystem;
@@ -26,14 +30,24 @@ class Main extends Sprite
 		Sys.setCwd(Path.addTrailingSlash(Context.getObbDir()));
 		#end
 
-		untyped __global__.__hxcpp_set_critical_error_handler(onCriticalError);
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
 
 		addChild(new FlxGame(1280, 720, PlayState));
 	}
 
-	private inline function onCriticalError(error:Dynamic):Void
+	private inline function onUncaughtError(event:UncaughtErrorEvent):Void
 	{
-		final log:Array<String> = [Std.isOfType(error, String) ? error : Std.string(error)];
+		event.preventDefault();
+		event.stopImmediatePropagation();
+
+		final log:Array<String> = [];
+
+		if (Std.isOfType(event.error, Error))
+			log.push(cast(event.error, Error).message);
+		else if (Std.isOfType(event.error, ErrorEvent))
+			log.push(cast(event.error, ErrorEvent).text);
+		else
+			log.push(Std.string(event.error));
 
 		for (item in CallStack.exceptionStack(true))
 		{
@@ -66,6 +80,6 @@ class Main extends Sprite
 
 		Log.trace(msg, null);
 		Lib.application.window.alert(msg, 'Error!');
-		System.exit(1);
+		LimeSystem.exit(1);
 	}
 }
