d095297244d5543e30ed6533791c3a6abe4a7258
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-01-14 22:46:20 +0200

    Using the handle.

--- source/hxvlc/openfl/Video.hx
+++ source/hxvlc/openfl/Video.hx
@@ -7,56 +7,19 @@ import haxe.io.BytesData;
 import haxe.io.Path;
 import haxe.Exception;
 import haxe.Int64;
+import hxvlc.libvlc.Handle;
 import hxvlc.libvlc.LibVLC;
 import hxvlc.libvlc.Types;
-import hxvlc.util.Define;
 import lime.app.Event;
 import lime.utils.Log;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.display3D.textures.Texture;
-#if linux
-import sys.FileSystem;
-#end
 
 using StringTools;
 
-#if android
-@:headerInclude('android/log.h')
-#end
 @:headerInclude('assert.h')
-@:headerInclude('stdarg.h')
-@:headerInclude('stdio.h')
 @:cppNamespaceCode('
-static void logging(void *data, int level, const libvlc_log_t *ctx, const char *fmt, va_list args)
-{
-	#if defined(HX_ANDROID)
-	switch (level)
-	{
-		case LIBVLC_DEBUG:
-			__android_log_vprint(ANDROID_LOG_DEBUG, "HXVLC", fmt, args);
-			break;
-		case LIBVLC_NOTICE:
-			__android_log_vprint(ANDROID_LOG_INFO, "HXVLC", fmt, args);
-			break;
-		case LIBVLC_WARNING:
-			__android_log_vprint(ANDROID_LOG_WARN, "HXVLC", fmt, args);
-			break;
-		case LIBVLC_ERROR:
-			__android_log_vprint(ANDROID_LOG_ERROR, "HXVLC", fmt, args);
-			break;
-	}
-	#else
-	char buffer[1024] = { 0 };
-
-	strcpy(buffer, fmt);
-
-	strcat(buffer, "\\n");
-
-	vprintf(buffer, args);
-	#endif
-}
-
 static void *lock(void *opaque, void **planes)
 {
 	Video_obj *self = reinterpret_cast<Video_obj *>(opaque);
@@ -284,8 +247,6 @@ class Video extends Bitmap
 	 */
 	public var onDisplay(default, null):Event<Void->Void>;
 
-	@:noCompletion private static var instance:cpp.RawPointer<LibVLC_Instance_T>;
-
 	@:noCompletion private var mediaItem:cpp.RawPointer<LibVLC_Media_T>;
 	@:noCompletion private var mediaPlayer:cpp.RawPointer<LibVLC_MediaPlayer_T>;
 	@:noCompletion private var eventManager:cpp.RawPointer<LibVLC_EventManager_T>;
@@ -318,7 +279,7 @@ class Video extends Bitmap
 		for (i in 0...8)
 			events[i] = false;
 
-		initInstance();
+		Handle.initInstance();
 	}
 
 	/**
@@ -332,19 +293,19 @@ class Video extends Bitmap
 	 */
 	public function load(location:String, repeat:UInt = 0, ?options:Array<String>):Bool
 	{
-		if (instance == null)
+		if (Handle.instance == null)
 			return false;
 
 		if (location != null && location.length > 0)
 		{
 			if (location.contains('://'))
-				mediaItem = LibVLC.media_new_location(instance, location);
+				mediaItem = LibVLC.media_new_location(Handle.instance, location);
 			else
 			{
 				#if windows
-				mediaItem = LibVLC.media_new_path(instance, Path.normalize(location).split('/').join('\\'));
+				mediaItem = LibVLC.media_new_path(Handle.instance, Path.normalize(location).split('/').join('\\'));
 				#else
-				mediaItem = LibVLC.media_new_path(instance, Path.normalize(location));
+				mediaItem = LibVLC.media_new_path(Handle.instance, Path.normalize(location));
 				#end
 			}
 		}
@@ -353,7 +314,7 @@ class Video extends Bitmap
 
 		if (mediaPlayer == null)
 		{
-			mediaPlayer = LibVLC.media_player_new(instance);
+			mediaPlayer = LibVLC.media_player_new(Handle.instance);
 
 			if (eventManager == null)
 			{
@@ -495,93 +456,6 @@ class Video extends Bitmap
 		eventManager = null;
 		mediaPlayer = null;
 		mediaItem = null;
-	}
-
-	/**
-	 * Initialize LibVLC instance.
-	 *
-	 * @return `true` if the instance created successfully or `false` if there's an error.
-	 */
-	public static function initInstance():Bool
-	{
-		if (instance == null)
-		{
-			#if (windows || macos)
-			Sys.putEnv('VLC_PLUGIN_PATH', Path.join([Path.directory(Sys.programPath()), 'plugins']));
-			#elseif linux
-			final pluginsPath:String = '/usr/local/lib/vlc/plugins';
-
-			if (FileSystem.exists(pluginsPath) && FileSystem.isDirectory(pluginsPath))
-				Sys.putEnv('VLC_PLUGIN_PATH', pluginsPath);
-			else if (FileSystem.exists(pluginsPath.replace('local/', '')) && FileSystem.isDirectory(pluginsPath.replace('local/', '')))
-				Sys.putEnv('VLC_PLUGIN_PATH', pluginsPath.replace('local/', ''));
-			#end
-
-			var args:cpp.StdVectorConstCharStar = cpp.StdVectorConstCharStar.alloc();
-
-			args.push_back("--drop-late-frames");
-			args.push_back("--intf=dummy");
-			args.push_back("--no-interact");
-			args.push_back("--no-lua");
-			args.push_back("--no-snapshot-preview");
-			args.push_back("--no-spu");
-			args.push_back("--no-stats");
-			args.push_back("--no-sub-autodetect-file");
-			args.push_back("--no-video-title-show");
-			args.push_back("--no-xlib");
-			#if (windows || macos)
-			args.push_back("--reset-config");
-			args.push_back("--reset-plugins-cache");
-			#elseif linux
-			final pluginsPath:String = Sys.getEnv('VLC_PLUGIN_PATH');
-
-			// Needs testing.
-			if (pluginsPath != null && pluginsPath.length > 0)
-				args.push_back("--reset-plugins-cache");
-			#end
-			args.push_back("--text-renderer=dummy");
-			args.push_back("--verbose=" + Define.getDefineInt('HXVLC_VERBOSE', 0));
-
-			instance = LibVLC.alloc(args.size(), untyped args.data());
-
-			if (instance == null)
-			{
-				final errmsg:String = cast(LibVLC.errmsg(), String);
-
-				if (errmsg != null && errmsg.length > 0)
-					Log.error('Failed to initialize the LibVLC instance, Error: $errmsg');
-				else
-					Log.error('Failed to initialize the LibVLC instance');
-
-				return false;
-			}
-			else
-			{
-				#if HXVLC_LOGGING
-				LibVLC.log_set(instance, untyped __cpp__('logging'), untyped __cpp__('NULL'));
-				#else
-				Log.info('LibVLC logging is being disabled');
-				#end
-			}
-		}
-
-		return true;
-	}
-
-	/**
-	 * Frees LibVLC instance.
-	 */
-	public static function disposeInstance():Void
-	{
-		if (instance != null)
-		{
-			#if HXVLC_LOGGING
-			LibVLC.log_unset(instance);
-			#end
-			LibVLC.release(instance);
-		}
-
-		instance = null;
 	}
 
 	// Get & Set Methods
