5b3cb0ecc00ff28a2a948ee2fa5c5b7d55247edc
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-05-03 14:07:15 +0000

    Nobody used the FlxVideoBackdrop anyways.

--- samples/flixel/source/PlayState.hx
+++ samples/flixel/source/PlayState.hx
@@ -10,7 +10,6 @@ import flixel.FlxG;
 import flixel.FlxState;
 import haxe.Exception;
 import hxvlc.flixel.FlxVideo;
-import hxvlc.flixel.FlxVideoBackdrop;
 import hxvlc.flixel.FlxVideoSprite;
 import hxvlc.util.Handle;
 import openfl.system.System;
