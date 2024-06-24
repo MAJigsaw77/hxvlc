738c987aa9b858b471f3f13ca13be6d95b00ac5c
Lily <egzozu.be.bas@gmail.com>
2024-06-25 00:43:42 +0300

    Add files via upload

--- docs/docs.bat
+++ docs/docs.bat
@@ -1,3 +1,3 @@
 @echo off
 haxe docs/docs.hxml
-haxelib run dox -theme ./docs/theme -i docs -o pages --title "hxVLC Documentation" -in "hxvlc" --toplevel-package hxvlc
+haxelib run dox -theme ./docs/theme -i docs -o pages --title "hxVLC Documentation" -in "hxvlc" --toplevel-package hxvlc -D source-path "https://github.com/MAJigsaw77/hxvlc/tree/main/source" -D logo "https://raw.githubusercontent.com/MAJigsaw77/hxvlc/main/logo_small.png"
