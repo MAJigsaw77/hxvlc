37f5651535369d69f6beea798b32eccfba21683e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-25 00:56:18 +0300

    Update Location.hx

--- source/hxvlc/util/Location.hx
+++ source/hxvlc/util/Location.hx
@@ -5,8 +5,11 @@ import hxvlc.util.typeLimit.OneOfThree;
 
 /**
  * Represents a location which can be:
+ *
  * - a local filesystem path or a media location URL.
+ *
  * - an open file descriptor ID.
+ *
  * - a bitstream input.
  */
 typedef Location = OneOfThree<String, Int, Bytes>;
