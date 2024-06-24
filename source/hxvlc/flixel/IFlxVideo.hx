22956ecfe9b577d76d776b8208e428c75d9e047e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-24 05:42:28 +0300

    Update IFlxVideo.hx

--- source/hxvlc/flixel/IFlxVideo.hx
+++ source/hxvlc/flixel/IFlxVideo.hx
@@ -8,20 +8,20 @@ import hxvlc.util.igenerals.IVideo;
  */
 interface IFlxVideo extends IVideo
 {
-    /**
+	/**
 	 * Whether the video should automatically pause when focus is lost.
 	 *
 	 * WARNING: Must be set before loading a video.
 	 */
 	public var autoPause:Bool;
 
-    /**
+	/**
 	 * The video's volume multiplier.
 	 */
-    public var volumeMultiplier:Float;
+	public var volumeMultiplier:Float;
 
-    #if FLX_SOUND_SYSTEM
-    /**
+	#if FLX_SOUND_SYSTEM
+	/**
 	 * Whether Flixel should automatically adjust the volume according to the Flixel sound system's current volume.
 	 */
 	public var autoVolumeHandle:Bool;
