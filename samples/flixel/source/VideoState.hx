81ab59516d9e0f425d99e4fc178e1f3717f61015
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-29 08:47:40 +0300

    Update and rename PlayState.hx to VideoState.hx

--- samples/flixel/source/VideoState.hx
+++ samples/flixel/source/VideoState.hx
@@ -11,7 +11,6 @@ import flixel.FlxG;
 import flixel.FlxState;
 import haxe.io.Path;
 import haxe.Exception;
-import hxvlc.externs.Types;
 import hxvlc.flixel.FlxVideo;
 import hxvlc.flixel.FlxVideoSprite;
 import hxvlc.util.Handle;
@@ -21,7 +20,7 @@ import openfl.Lib;
 import sys.io.File;
 import sys.FileSystem;
 
-class PlayState extends FlxState
+class VideoState extends FlxState
 {
 	var video:FlxVideoSprite;
 	var videoPositionBar:FlxBar;
@@ -34,46 +33,7 @@ class PlayState extends FlxState
 
 		FlxG.cameras.bgColor = 0xFF131C1B;
 
-		var metadataInfo:FlxText = new FlxText(0, 40, 0, 'Title Unknown\nArtist Unknown', 32);
-		metadataInfo.setBorderStyle(OUTLINE, FlxColor.BLACK);
-		metadataInfo.borderSize = 2;
-		metadataInfo.alignment = CENTER;
-		metadataInfo.screenCenter(X);
-
 		video = new FlxVideoSprite(0, 0);
-		video.bitmap.onMediaParsedChanged.add(function(status:Int):Void
-		{
-			switch (status)
-			{
-				case status if (status == LibVLC_Media_Parsed_Status_Skipped):
-					FlxG.log.notice('Media parsing skipped.');
-
-					video.bitmap.parseStop();
-
-					FlxTimer.wait(0.001, function():Void
-					{
-						video.play();
-					});
-				case status if (status == LibVLC_Media_Parsed_Status_Failed):
-					FlxG.log.notice('Media parsing failed. Stopping further processing.');
-
-					video.bitmap.parseStop();
-				case status if (status == LibVLC_Media_Parsed_Status_Timeout):
-					FlxG.log.notice('Media parsing timed out. Stopping further processing.');
-
-					video.bitmap.parseStop();
-				case status if (status == LibVLC_Media_Parsed_Status_Done):
-					FlxG.log.notice('Media parsing done. Starting playback.');
-
-					metadataInfo.text = '${video.bitmap.getMeta(LibVLC_Meta_Title) ?? 'Title Unknown'}\n${video.bitmap.getMeta(LibVLC_Meta_Artist) ?? 'Artist Unknown'}';
-					metadataInfo.screenCenter(X);
-
-					FlxTimer.wait(0.001, function():Void
-					{
-						video.play();
-					});
-			}
-		});
 		video.bitmap.onFormatSetup.add(function():Void
 		{
 			video.setGraphicSize(FlxG.width * 0.8, FlxG.height * 0.8);
@@ -90,14 +50,6 @@ class PlayState extends FlxState
 		video.antialiasing = true;
 		add(video);
 
-		final parseLocal:Int = LibVLC_Media_Parse_Local;
-		final fetchLocal:Int = LibVLC_Media_Fetch_Local;
-
-		video.parseWithOptions(parseLocal | fetchLocal, -1);
-
-		metadataInfo.antialiasing = true;
-		add(metadataInfo);
-
 		var libvlcVersion:FlxText = new FlxText(10, FlxG.height - 30, 0, 'LibVLC ${Handle.version}', 16);
 		libvlcVersion.setBorderStyle(OUTLINE, FlxColor.BLACK);
 		libvlcVersion.active = false;
@@ -107,6 +59,11 @@ class PlayState extends FlxState
 		videoPositionBar = new FlxBar(10, FlxG.height - 50, LEFT_TO_RIGHT, FlxG.width - 20, 10, null, '', 0, 1);
 		videoPositionBar.createFilledBar(FlxColor.GRAY, FlxColor.CYAN, true, FlxColor.BLACK);
 		add(videoPositionBar);
+
+		FlxTimer.wait(0.001, function():Void
+		{
+			video.play();
+		});
 
 		super.create();
 	}
