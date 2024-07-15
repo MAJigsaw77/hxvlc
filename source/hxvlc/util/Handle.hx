d806d3d6cd240a3dbf73dbee19a46e454ff371cf
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-15 20:54:35 +0300

    Update Handle.hx

--- source/hxvlc/util/Handle.hx
+++ source/hxvlc/util/Handle.hx
@@ -44,31 +44,43 @@ static void instance_logging(void *data, int level, const libvlc_log_t *ctx, con
 			__android_log_vprint(ANDROID_LOG_WARN, "HXVLC", fmt, args);
 			break;
 		case LIBVLC_DEBUG:
+			printf("[DEBUG] ");
+			vprintf(fmt, args);
+			printf("\\n");
+			break;
 		default:
-			__android_log_vprint(ANDROID_LOG_DEBUG, "HXVLC", fmt, args);
+			__android_log_vprint(ANDROID_LOG_UNKNOWN, "HXVLC", fmt, args);
 			break;
 	}
 	#else
 	switch (level)
 	{
 		case LIBVLC_NOTICE:
-			printf("[NOT] ");
+			printf("[NOTICE] ");
+			vprintf(fmt, args);
+			printf("\\n");
 			break;
 		case LIBVLC_ERROR:
-			printf("[ERR] ");
+			printf("[ERROR] ");
+			vprintf(fmt, args);
+			printf("\\n");
 			break;
 		case LIBVLC_WARNING:
-			printf("[WAR] ");
+			printf("[WARNING] ");
+			vprintf(fmt, args);
+			printf("\\n");
 			break;
 		case LIBVLC_DEBUG:
+			printf("[DEBUG] ");
+			vprintf(fmt, args);
+			printf("\\n");
+			break;
 		default:
-			printf("[DEB] ");
+			printf("[UNKNOWN] ");
+			vprintf(fmt, args);
+			printf("\\n");
 			break;
 	}
-
-	vprintf(fmt, args);
-
-	printf("\\n");
 	#endif
 
 	hx::SetTopOfStack((int *)0, true);
