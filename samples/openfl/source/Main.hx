65ba4706005befc3f4d74328eacd0058f4d6884e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-22 13:59:18 +0300

    Update Main.hx

--- samples/openfl/source/Main.hx
+++ samples/openfl/source/Main.hx
@@ -3,23 +3,12 @@ package;
 #if android
 import android.content.Context;
 import android.os.Build;
-import android.widget.Toast;
 #end
 import haxe.io.Path;
-import haxe.CallStack;
-import haxe.Exception;
 import hxvlc.openfl.Video;
-import lime.system.System;
-import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.errors.Error;
-import openfl.events.ErrorEvent;
-import openfl.events.UncaughtErrorEvent;
 import openfl.events.Event;
-import openfl.utils.Assets;
 import openfl.Lib;
-import sys.io.File;
-import sys.FileSystem;
 
 using StringTools;
 
@@ -35,13 +24,23 @@ class Main extends Sprite
 		Sys.setCwd(System.documentsDirectory);
 		#end
 
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
 		Lib.current.addChild(new Main());
 	}
 
 	public function new():Void
 	{
 		super();
+
+		if (stage != null)
+			onAddedToStage();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+
+	private function onAddedToStage(?event:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+			removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
 
 		var refreshRate:Int = Lib.application.window.displayMode.refreshRate;
 
@@ -108,53 +107,5 @@ class Main extends Sprite
 	private inline function stage_onDeactivate(event:Event):Void
 	{
 		video.pause();
-	}
-
-	private static inline function onUncaughtError(event:UncaughtErrorEvent):Void
-	{
-		event.preventDefault();
-		event.stopImmediatePropagation();
-
-		final log:Array<String> = [];
-
-		if (Std.isOfType(event.error, Error))
-			log.push(cast(event.error, Error).message);
-		else if (Std.isOfType(event.error, ErrorEvent))
-			log.push(cast(event.error, ErrorEvent).text);
-		else
-			log.push(Std.string(event.error));
-
-		for (item in CallStack.exceptionStack(true))
-		{
-			switch (item)
-			{
-				case CFunction:
-					log.push('C Function');
-				case Module(m):
-					log.push('Module [$m]');
-				case FilePos(s, file, line, column):
-					log.push('$file [line $line]');
-				case Method(classname, method):
-					log.push('$classname [method $method]');
-				case LocalFunction(name):
-					log.push('Local Function [$name]');
-			}
-		}
-
-		final msg:String = log.join('\n');
-
-		try
-		{
-			if (!FileSystem.exists('errors'))
-				FileSystem.createDirectory('errors');
-
-			File.saveContent('errors/' + Date.now().toString().replace(' ', '-').replace(':', "'") + '.txt', msg);
-		}
-		catch (e:Exception)
-			Sys.println('Couldn\'t save error message "${e.message}"');
-
-		Sys.println(msg);
-		Lib.application.window.alert(msg, 'Error!');
-		System.exit(1);
 	}
 }
