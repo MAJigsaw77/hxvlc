76082f44922317334adca0b1bb92a13a2ebebac6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-22 13:55:04 +0300

    Update Main.hx

--- samples/flixel/source/Main.hx
+++ samples/flixel/source/Main.hx
@@ -8,20 +8,11 @@ import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxGame;
 import haxe.io.Path;
-import haxe.CallStack;
-import haxe.Exception;
-import haxe.Log;
 import lime.system.System;
 import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.errors.Error;
-import openfl.events.ErrorEvent;
-import openfl.events.UncaughtErrorEvent;
+import openfl.events.Event;
 import openfl.Lib;
-import sys.io.File;
-import sys.FileSystem;
-
-using StringTools;
 
 class Main extends Sprite
 {
@@ -35,12 +26,23 @@ class Main extends Sprite
 		Sys.setCwd(System.documentsDirectory);
 		#end
 
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onUncaughtError);
+		Lib.current.addChild(new Main());
 	}
 
 	public function new():Void
 	{
 		super();
+
+		if (stage != null)
+			onAddedToStage();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+
+	private function onAddedToStage(?event:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+			removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
 
 		FlxG.signals.gameResized.add(onResizeGame);
 
@@ -60,55 +62,7 @@ class Main extends Sprite
 		FlxG.game.addChild(fps);
 	}
 
-	private static inline function onUncaughtError(event:UncaughtErrorEvent):Void
-	{
-		event.preventDefault();
-		event.stopImmediatePropagation();
-
-		final log:Array<String> = [];
-
-		if (Std.isOfType(event.error, Error))
-			log.push(cast(event.error, Error).message);
-		else if (Std.isOfType(event.error, ErrorEvent))
-			log.push(cast(event.error, ErrorEvent).text);
-		else
-			log.push(Std.string(event.error));
-
-		for (item in CallStack.exceptionStack(true))
-		{
-			switch (item)
-			{
-				case CFunction:
-					log.push('C Function');
-				case Module(m):
-					log.push('Module [$m]');
-				case FilePos(s, file, line, column):
-					log.push('$file [line $line]');
-				case Method(classname, method):
-					log.push('$classname [method $method]');
-				case LocalFunction(name):
-					log.push('Local Function [$name]');
-			}
-		}
-
-		final msg:String = log.join('\n');
-
-		try
-		{
-			if (!FileSystem.exists('errors'))
-				FileSystem.createDirectory('errors');
-
-			File.saveContent('errors/' + Date.now().toString().replace(' ', '-').replace(':', "'") + '.txt', msg);
-		}
-		catch (e:Exception)
-			Sys.println('Couldn\'t save error message "${e.message}"');
-
-		Sys.println(msg);
-		Lib.application.window.alert(msg, 'Error!');
-		System.exit(1);
-	}
-
-	private inline function onResizeGame(width:Int, height:Int):Void
+	private function onResizeGame(width:Int, height:Int):Void
 	{
 		final scale:Float = Math.min(FlxG.stage.stageWidth / FlxG.width, FlxG.stage.stageHeight / FlxG.height);
 
