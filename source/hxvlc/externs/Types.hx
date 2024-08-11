66e9e62cbfbefdb33fac56bbdcc71e97f27995a1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-12 01:26:01 +0300

    Update Types.hx

--- source/hxvlc/externs/Types.hx
+++ source/hxvlc/externs/Types.hx
@@ -461,6 +461,13 @@ typedef LibVLC_Audio_Flush_CB = cpp.Callable<(data:cpp.RawPointer<cpp.Void>, pts
 typedef LibVLC_Audio_Drain_CB = cpp.Callable<(data:cpp.RawPointer<cpp.Void>) -> Void>;
 
 @:dox(hide)
+typedef LibVLC_Video_Format_CB = cpp.Callable<(opaque:cpp.RawPointer<cpp.RawPointer<cpp.Void>>, chroma:cpp.CharStar, width:cpp.RawPointer<cpp.UInt32>,
+		height:cpp.RawPointer<cpp.UInt32>, pitches:cpp.RawPointer<cpp.UInt32>, lines:cpp.RawPointer<cpp.UInt32>) -> cpp.UInt32>;
+
+@:dox(hide)
+typedef LibVLC_Video_Cleanup_CB = cpp.Callable<(opaque:cpp.RawPointer<cpp.Void>) -> Void>;
+
+@:dox(hide)
 typedef LibVLC_Audio_Set_Volume_CB = cpp.Callable<(data:cpp.RawPointer<cpp.Void>, volume:Single, mute:Bool) -> Void>;
 
 @:dox(hide)
