cef7dfd0e9d2341939e7fca37d5fdc42fc0b6f95
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-13 23:25:24 +0200

    Update CheckMacro.hx

--- source/hxvlc/macros/CheckMacro.hx
+++ source/hxvlc/macros/CheckMacro.hx
@@ -8,7 +8,7 @@ class CheckMacro
 {
 	public static function run():Void
 	{
-		if (!Context.defined('cpp') && (!Context.defined('desktop') && !Context.defined('mobile')))
+		if (!Context.defined('cpp') && !(Context.defined('desktop') || Context.defined('mobile')))
 			Context.fatalError('The current target platform isn\'t supported by hxvlc.', Context.currentPos());
 
 		if (Context.defined('hxCodec'))
