cf5ed8863156ca648f080bd792709c1d9fbffa68
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-11-13 23:19:14 +0200

    Create CheckMacro.hx

---
+++ source/hxvlc/macros/CheckMacro.hx
@@ -0,0 +1,18 @@
+package hxvlc.macros;
+
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Compiler;
+
+class CheckMacro
+{
+	public static function run():Void
+	{
+		if (!Context.defined('cpp') && (!Context.defined('desktop') && !Context.defined('mobile')))
+			Context.fatalError('The current target platform isn\'t supported by hxvlc.', Context.currentPos());
+
+		if (Context.defined('hxCodec'))
+			Context.fatalError('hxvlc and hxCodec cannot be used in the same project..', Context.currentPos());
+	}
+}
+#end
