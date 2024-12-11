69b136a6428c190e16200aca254096e5af6c5671
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-11 12:48:04 +0200

    Update StdVector.hx

--- source/cpp/StdVector.hx
+++ source/cpp/StdVector.hx
@@ -7,19 +7,17 @@ package cpp;
 @:structAccess
 extern class StdVector<T> implements ArrayAccess<Reference<T>>
 {
-	overload function new():Void;
-	overload function new(size:Int):Void;
+	@:overload(function(size:Int):Void {})
+	function new():Void;
 
 	function at(index:Int):T;
 	function front():T;
 	function back():T;
 	function data():RawPointer<T>;
-
 	function empty():Bool;
 	function size():Int;
 	function capacity():Int;
 	function reserve(newCapacity:Int):Void;
-
 	function clear():Void;
 	function push_back(value:T):Void;
 	function pop_back():Void;
