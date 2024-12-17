27a92db96924470cd84d1bda9769441c79f22348
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-17 08:05:47 +0200

    Set the minimum required `lime` version to `8.0.1`.
    This is done because lime added support for `MainLoop` within `NativeApplication` in this version.

--- source/hxvlc/macros/CheckMacro.hx
+++ source/hxvlc/macros/CheckMacro.hx
@@ -8,10 +8,14 @@ class CheckMacro
 	public static function run():Void
 	{
 		if (!Context.defined('cpp') && !(Context.defined('desktop') || Context.defined('mobile')))
-			Context.fatalError('The current target platform isn\'t supported by hxvlc.', Context.currentPos());
+			Context.fatalError('The current target platform isn\'t supported by the hxvlc library.', (macro null).pos);
+
+		#if (lime < version("8.0.1"))
+		Context.fatalError('The hxvlc library requires Lime version 8.0.1 or newer. Please update Lime by running `haxelib update lime`.', (macro null).pos);
+		#end
 
 		if (Context.defined('hxCodec'))
-			Context.fatalError('hxvlc and hxCodec cannot be used in the same project..', Context.currentPos());
+			Context.fatalError('The hxvlc library and hxCodec cannot be used in the same project.', (macro null).pos);
 	}
 }
 #end
