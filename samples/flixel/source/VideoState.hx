e12057994c9cc51d1586b4125e2139a7d6cb338f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-01-03 15:47:44 +0200

    Update VideoState.hx

--- samples/flixel/source/VideoState.hx
+++ samples/flixel/source/VideoState.hx
@@ -3,7 +3,6 @@ package;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
-import flixel.ui.FlxBar;
 import flixel.FlxG;
 import flixel.FlxState;
 import hxvlc.flixel.FlxVideo;
@@ -14,29 +13,22 @@ import sys.FileSystem;
 class VideoState extends FlxState
 {
 	var video:FlxVideoSprite;
-	var videoPositionBar:FlxBar;
 
 	override function create():Void
 	{
-		FlxG.cameras.bgColor = 0xFF131C1B;
-
 		video = new FlxVideoSprite(0, 0);
+		video.active = false;
 		video.antialiasing = true;
 		video.bitmap.onFormatSetup.add(function():Void
 		{
 			if (video.bitmap != null && video.bitmap.bitmapData != null)
 			{
-				final scale:Float = Math.min(FlxG.width / video.bitmap.bitmapData.width, FlxG.height / video.bitmap.bitmapData.height) * 0.8;
+				final scale:Float = Math.min(FlxG.width / video.bitmap.bitmapData.width, FlxG.height / video.bitmap.bitmapData.height);
 
 				video.setGraphicSize(video.bitmap.bitmapData.width * scale, video.bitmap.bitmapData.height * scale);
 				video.updateHitbox();
 				video.screenCenter();
 			}
-		});
-		video.bitmap.onPositionChanged.add(function(position:Single):Void
-		{
-			if (videoPositionBar != null)
-				videoPositionBar.value = position;
 		});
 		video.bitmap.onEndReached.add(video.destroy);
 
@@ -59,16 +51,11 @@ class VideoState extends FlxState
 		add(video);
 
 		final libvlcVersion:FlxText = new FlxText(10, FlxG.height - 30, 0, 'LibVLC ${Handle.version}', 16);
+		libvlcVersion.font = FlxAssets.FONT_DEBUGGER;
 		libvlcVersion.setBorderStyle(OUTLINE, FlxColor.BLACK);
 		libvlcVersion.active = false;
 		libvlcVersion.antialiasing = true;
 		add(libvlcVersion);
-
-		videoPositionBar = new FlxBar(10, FlxG.height - 50, LEFT_TO_RIGHT, FlxG.width - 20, 10, null, '', 0, 1);
-		videoPositionBar.createFilledBar(FlxColor.GRAY, FlxColor.CYAN, true, FlxColor.BLACK);
-		videoPositionBar.antialiasing = true;
-		videoPositionBar.numDivisions = 999999;
-		add(videoPositionBar);
 
 		FlxTimer.wait(0.001, function():Void
 		{
