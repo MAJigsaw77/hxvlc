65350a30c848903f306bfcbc35c622c1594bcbd5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-01-07 17:02:41 +0200

    Update VideoState.hx

--- samples/flixel/source/VideoState.hx
+++ samples/flixel/source/VideoState.hx
@@ -91,10 +91,15 @@ class VideoState extends FlxState
 			#if HXVLC_ENABLE_STATS
 			if (video != null && video.bitmap != null && video.bitmap.stats != null)
 				fpsInfo.text = 'FPS ${fps.currentFPS}\n${video.bitmap.stats.toString()}';
+			else if (Handle.outputModules != null)
+				fpsInfo.text = 'FPS ${fps.currentFPS}\n${Handle.outputModules.toString()}';
 			else
 				fpsInfo.text = 'FPS ${fps.currentFPS}';
 			#else
-			fpsInfo.text = 'FPS ${fps.currentFPS}';
+			if (Handle.outputModules != null)
+				fpsInfo.text = 'FPS ${fps.currentFPS}\n${Handle.outputModules.toString()}';
+			else
+				fpsInfo.text = 'FPS ${fps.currentFPS}';
 			#end
 		}
 
